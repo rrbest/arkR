454ed4fb313783c44e636d2b1b5754fc0aa3c089
ryano <ryano1110@gmail.com>
2017-05-09 19:20:33 -0700

    fixed NAMESPACE

--- R/account_balanace.R
+++ R/account_balanace.R
@@ -2,3 +2,5 @@ ark.accountBalance <- function(address){
   url <- paste0(options$url, "/api/accounts/getBalance/?address=", address)
   as.numeric(content(GET(url = url))$balance) / options$scaling
 } # example: Account.getBalance(address)
+
+
