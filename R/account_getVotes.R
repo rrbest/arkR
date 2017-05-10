072a1bb4fa8787326bfe69e46bdcfe2de1f07dca
ryano <ryano1110@gmail.com>
2017-05-09 21:48:59 -0700

    fixed function name for getVotes

--- R/account_getVotes.R
+++ R/account_getVotes.R
@@ -1,6 +1,6 @@
 #' @export
 #' @rdname account_getVotes
-ark.getVotes <- function(address){
+getVotes <- function(address){
   url <- paste0(options$url, "/api/accounts/delegates/?address=", address)
   content(GET(url = url))
 } # example: ark.getVotes(address)
