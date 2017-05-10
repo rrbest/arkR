1e1e8da4efdb7d89da6bcd682ca2acfd6ca3e1c7
ryano <ryano1110@gmail.com>
2017-05-09 21:43:51 -0700

    added export tag

--- R/account_getBalanace.R
+++ R/account_getBalanace.R
@@ -1,3 +1,4 @@
+#' @export
 getBalance <- function(address){
   url <- paste0(options$url, "/api/accounts/getBalance/?address=", address)
   as.numeric(content(GET(url = url))$balance) / options$scaling
