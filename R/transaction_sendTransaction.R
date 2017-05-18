0f78529cfc30b60f2d773dfac9d0506d5eba8c1a
ryano <ryano1110@gmail.com>
2017-05-17 20:16:51 -0700

    disabled sendTransactions

--- R/transaction_sendTransaction.R
+++ R/transaction_sendTransaction.R
@@ -1,19 +1,15 @@
-sendTransaction = function(secretKey, secondSecretKey = NULL, amount, to) {
-  data = list(
-    secret = secretKey,
-    amount =  amount * options$scaling,
-    recipientId = to
-  )
-
-  if(!is.null(secondSecretKey)){
-    data['secondSecret'] = secondSecretKey
-  }
-
-  url <- paste0(options$url, "/api/transactions")
-  content(PUT(url, body = data, encode='json'))
-}
-
-# example:
-#   sendTransaction(secretKey = "one two three four five six seven eight nine ten eleven twelve",
-#                   to = address,
-#                   amount = 1)
+# ! Temporarily Disabled !
+#
+# sendTransaction = function(secretKey, secondSecretKey = NULL, amount, to) {
+#   data = list(
+#     username = username
+#   )
+#
+#   url <- paste0(options$url, "/api/transactions")
+#   content(PUT(url, body = data, encode='json'))
+# }
+#
+# # example:
+# #   sendTransaction(secretKey = "one two three four five six seven eight nine ten eleven twelve",
+# #                   to = address,
+# #                   amount = 1)
